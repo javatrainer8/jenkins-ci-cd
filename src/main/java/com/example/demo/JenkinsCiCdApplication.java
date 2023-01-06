ec2bf8350afeff3242a910ae7afc167455a08230
lucky <javatrainer8@gmail.com>
2023-01-06 22:28:10 +0530

    added sysout

--- src/main/java/com/example/demo/JenkinsCiCdApplication.java
+++ src/main/java/com/example/demo/JenkinsCiCdApplication.java
@@ -8,6 +8,7 @@ public class JenkinsCiCdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsCiCdApplication.class, args);
+		System.out.println("ok jenkins...........");
 	}
 
 }
